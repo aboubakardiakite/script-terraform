288cccca81e123c37ad8f88e581b48a2b66e5ff8
aboubakar DIAKITE <aboubakarsiriki60@gmail.com>
2025-03-24 12:05:56 +0100

    new archi

--- main.tf
+++ main.tf
@@ -13,14 +13,14 @@ provider "kubernetes" {
 
 # React Frontend
 resource "kubernetes_manifest" "react_deployment" {
-  manifest = yamldecode(file("${path.module}/k8s/react-deployment.yaml"))
+  manifest = yamldecode(file("${path.module}/k8s/react-deployment.yml"))
   lifecycle {
     ignore_changes = all
   }
 }
 
 resource "kubernetes_manifest" "react_service" {
-  manifest = yamldecode(file("${path.module}/k8s/react-service.yaml"))
+  manifest = yamldecode(file("${path.module}/k8s/react-service.yml"))
   lifecycle {
     ignore_changes = all
   }
@@ -28,14 +28,14 @@ resource "kubernetes_manifest" "react_service" {
 
 # NestJS Backend API
 resource "kubernetes_manifest" "workout-planner" {
-  manifest = yamldecode(file("${path.module}/k8s/workout-planner.yaml"))
+  manifest = yamldecode(file("${path.module}/k8s/workout-planner.yml"))
   lifecycle {
     ignore_changes = all
   }
 }
 
 resource "kubernetes_manifest" "workout-planner_service" {
-  manifest = yamldecode(file("${path.module}/k8s/workout-planner-service.yaml"))
+  manifest = yamldecode(file("${path.module}/k8s/workout-planner-service.yml"))
   lifecycle {
     ignore_changes = all
   }
@@ -43,14 +43,14 @@ resource "kubernetes_manifest" "workout-planner_service" {
 
 # MongoDB
 resource "kubernetes_manifest" "mongodb_deployment" {
-  manifest = yamldecode(file("${path.module}/k8s/mongodb-deployment.yaml"))
+  manifest = yamldecode(file("${path.module}/k8s/mongodb-deployment.yml"))
   lifecycle {
     ignore_changes = all
   }
 }
 
 resource "kubernetes_manifest" "mongodb_service" {
-  manifest = yamldecode(file("${path.module}/k8s/mongodb-service.yaml"))
+  manifest = yamldecode(file("${path.module}/k8s/mongodb-service.yml"))
   lifecycle {
     ignore_changes = all
   }
